8e1bae8187cbac50a575bc5ae127e437970bb166
Paula Besson <paula.besson@myunidays.com>
2016-05-12 17:29:05 +0100

    Started adding unit tests for UserValidator class

---
+++ UNiDAYSHomework.Test/UserValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation.TestHelper;
+using NUnit.Framework;
+using UNiDAYSHomework.Models;
+
+namespace UNiDAYSHomework.Test
+{
+    [TestFixture]
+    class UserValidatorTests
+    {
+        private UserValidator validator;
+
+        [SetUp]
+        public void Setup()
+        {
+            validator = new UserValidator();
+        }
+
+
+        [Test]
+        public void TestNullEmailIsInvalid()
+        {
+            
+        }
+
+        public void TestEmailWithoutAtIsInvalid()
+        {
+
+        }
+
+    }
+}
--- UNiDAYSHomework/Models/UserValidator.cs
+++ UNiDAYSHomework/Models/UserValidator.cs
@@ -7,11 +7,11 @@ namespace UNiDAYSHomework.Models
         public UserValidator()
         {
             RuleFor(user => user.EmailAddress)
-                .NotNull().WithMessage("Email address must be supplied.")
+                .NotEmpty().WithMessage("Email address must be supplied.")
                 .EmailAddress().WithMessage("Invalid email address.");
 
             RuleFor(user => user.Password)
-                .NotNull().WithMessage("Password is required.")
+                .NotEmpty().WithMessage("Password is required.")
                 .Length(6, 32).WithMessage("Password must be between 6 and 32 characters long.");
         }
     }
